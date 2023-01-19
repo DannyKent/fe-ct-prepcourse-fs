56ea643b59f1ed884a882df33058bca5268a00d6
unknown <vidaniel057@gmail.com>
2023-01-18 23:16:08 -0500

    Ejercicios Resueltos del 1 al 3

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,11 +6,13 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   return Math.pow (num, 2);
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   return Math.pow(num, 3);
 }
 
 function elevar(num, exponent) {
